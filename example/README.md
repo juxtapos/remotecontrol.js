83e456f2dda0a87d968dd4f2f8962630a91e7d82
juxtapos <claus@formatvorlage.de>
2012-06-20 05:43:10 +0200

    moved lib files to npm package

--- example/README.md
+++ example/README.md
@@ -1,1 +1,1 @@
-
+The files required by the two example pages are copied by the npm postinstall script. 
--- node/bin/postinstall.sh
+++ node/bin/postinstall.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 cp node_modules/socket.io/node_modules/socket.io-client/dist/socket.io.min.js ../example/socket.io.min.js
+cp lib/remotecontrol.js.host.js ../example/remotecontrol.js.host.js
+cp lib/remotecontrol.js.remote.js ../example/remotecontrol.js.remote.js
