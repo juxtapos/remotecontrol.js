2422acdd2a664caa0b20edbd1b589f9a20577af7
juxtapos <claus@formatvorlage.de>
2012-06-21 01:27:26 +0200

    reorg files

--- node/bin/postinstall.sh
+++ node/bin/postinstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cp node_modules/socket.io-client/dist/socket.io.min.js ../example/socket.io.min.js
+cp node_modules/socket.io-client/dist/socket.io.min.js ../example/dist/socket.io.min.js
 cp -r lib/client/ ../example/dist/
