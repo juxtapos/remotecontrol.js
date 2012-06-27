bfd63690805cc985f3285d67226ba86f8448a760
juxtapos <claus@formatvorlage.de>
2012-06-27 11:31:05 +0200

    this and that

--- node/server.js
+++ node/server.js
@@ -2,101 +2,108 @@ var socketio = require('socket.io'),
 	crypto = require('crypto'),
 	io = socketio.listen(1337),
 	tokens = {},
-	TOKEN_TTL = 60000;
+	sockets = {},
+	TOKEN_TTL = 60000,
+	DEBUG_LEVEL = 1; // 3 for full message logging
 
+//
+// Configure socket.io.
+// 
 io.configure(function(){
     io.enable('browser client etag');
-    io.set('log level', 1); // 3 for full message logging
-    io.set('debug', true); // set to true for testing, sends token automatically to next connecting client. 
+    io.set('log level', DEBUG_LEVEL);
 });
 
+/**
+ * Creates a tokenId from calling createKey and cutting a random substring of length 5.
+ *
+ * @returns {String} token ID
+ */
 function createTokenId() {
-	var salt = "AddS417",
-		sha = crypto.createHash('sha1').update(Math.random() + new Date().getTime() + salt).digest('hex'), 
-		offset = parseInt(Math.random() * (sha.length - 5))
-	return sha.substring(offset, offset + 5);
+	var key = createKey(),
+		offset = parseInt(Math.random() * (key.length - 5))
+	return key.substring(offset, offset + 5);
 }
 
-
-var last_token_danger = null;
-
-var sockets = {};
+/**
+ * Creates a SHA1 hash, using a random value, unix epoch seconds and a salt string as input. 
+ * 
+ * @returns {String} SHA1 hash value. 
+ */
+function createKey() {
+	var salt = 'AddS417',
+		sha = crypto.createHash('sha1').update(Math.random() + new Date().getTime() + salt).digest('hex');
+	return sha;
+}
 
 io.sockets.on('connection', function (socket) {
-	console.log('client connected')
+	//console.log('socket connected ' + socket.id);
 
-	if (io.get('debug')) {
-		socket.emit('last_token_danger', { last_token_danger: last_token_danger } );
+	sockets[socket.id] = {
+		tokenId: null,
+		socket: socket
 	}
 
-/*
-	if (!sockets[socket.id]) {
-		console.log('add socket');
-		sockets[socket.id] = socket;
-	}*/
-
-	socket.on('disconnect', function () {
-		console.log('disconnect');
-		console.log(this.id)
-		if (socket.get('remoteTarget', function (err, obj) {
-			console.log('remoteTarget');console.log(obj);
-			obj && obj.emit('disconnect', { error: 'remote host disconnected' } );
-		}));
-	});
-
-	socket.on('host:getToken', function (data) {
-		var token = createTokenId();
-		tokens[token] = {
-			timeStamp: new Date().getTime(),
-			sourceSocket: socket,
+	socket.on('disconnect', function disconnect () {
+		//console.log('client disconnected ' + socket.id);
+		if (sockets[socket.id]) {
+			if (sockets[socket.id].tokenId) {
+				delete tokens[sockets[socket.id].tokenId];
+			}
+			delete sockets[socket.id];
 		}
-
-		last_token_danger = token;
-
-		socket.emit('receiveToken', { tokenId: token });
 	});
 
-	
+	socket.on('rcjs:requestToken', function (data) {
+		if (tokens[sockets[socket.id].tokenId]) {
+			delete tokens[sockets[socket.id].tokenId];
+		}
+		var tokenId = createTokenId();
+		sockets[socket.id].tokenId = tokenId;
+		tokens[tokenId] = {
+			timeStamp: new Date().getTime(),
+			receiverId: socket.id
+		};
+		socket.emit('rcjs:token', { tokenId: tokenId } );
+	});
 
-	socket.on('remote:supplyToken', function (data) {
-		if (tokens[data.tokenId]) {
-			var token = tokens[data.tokenId];
-			if (token.targetSocket) {
-				socket.emit('supplyToken', { error: "invalid token (already in use)" } );
+	// Request: from sender app to connect to receiver app using a valid tokenId. 
+    // Response: on error sends rcjs:supplyToken message back to source, on success a 
+    // rcjs:registerSender
+	socket.on('rcjs:supplyToken', function (data) {
+		var token;
+		if ( (token = tokens[data.tokenId]) ) {
+			var receiver = sockets[token.receiverId].socket;
+			if (receiver.sender) {
+				socket.emit('rcjs:supplyToken', { error: 'invalid token (already in use)' } );
 			} else if (new Date().getTime() > token.timeStamp + TOKEN_TTL) {
-				socket.emit('supplyToken', { error: "invalid token (expired)" } );
+				socket.emit('rcjs:supplyToken', { error: 'invalid token (expired)' } );
 				delete tokens[data.tokenId];
 			} else {
-				token.targetSocket = this;
-				token.sourceSocket.emit('register', { tokenId: data.tokenId } );
+				token.senderId = socket.id;
+				receiver.emit('rcjs:registerSender', { tokenId: data.tokenId } );
 			}
 		} else {
-			socket.emit('supplyToken', { error: "invalid token (unknown)" } );
+			socket.emit('rcjs:supplyToken', { error: 'invalid token (unknown)' } );
 		}
 	});
 
-	socket.on('*', function () {
-		console.log('yoghurt');
-
+	socket.on('rcjs:confirmRegistration', function (data) {
+		var token = tokens[data.tokenId];
+		token.key = createKey();
+		var sender = sockets[token.senderId].socket;
+		sender.emit('rcjs:startCapture', { tokenId: data.tokenId, key: token.key, events: data.events } );
 	});
 
-	socket.on('confirmRegister', function (data) {
-		tokens[data.tokenId].targetSocket.set('remoteTarget', this);
-		tokens[data.tokenId].targetSocket.emit('confirmRegister', { tokenId: data.tokenId } );
-	});
-
-	// 
-	// Register handlers for all gesture and touch event messages coming from the socket
-	[
-		'event:MSPointerDown', 'event:MSPointerMove', 'event:MSPointerUp', 
-		'event:MSGestureStart', 'event:MSGestureChange', 'event:MSGestureEnd', 
-		'event:gesturestart', 'event:gesturechange', 'event:gestureend', 
-		'event:touchstart', 'event:touchmove','event:touchend'
-	].forEach(function (type) {
-		socket.on(type, function (data) {
-			this.get('remoteTarget', function (err, obj) {
-				obj.emit(type, data);	
-			});
-		});
+	socket.on('rcjs:event', function (data) {
+		var token = tokens[data.tokenId];
+		if (!token) {
+			socket.emit('rcjs:receiverDisconnect', { error: 'tokenId missing, disconnect.' } );
+		} else if (data.key == token.key) {
+			var receiver = sockets[token.receiverId].socket;
+			receiver.emit('rcjs:event', { type: data.type, event: data.event } );
+		} else {
+			socket.emit('rcjs:invalid', { error: 'invalid key' } );
+		}
 	});
 });
