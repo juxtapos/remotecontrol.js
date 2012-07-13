c56b0d0aae87c90df18d116f9eebc9073cdae981
juxtapos <claus@formatvorlage.de>
2012-07-13 15:11:52 +0200

    improved connection handling and user messages

--- web/js/remotecontrol.js.remote.js
+++ web/js/remotecontrol.js.remote.js
@@ -10,26 +10,27 @@ function RemoteControl (options) {
 	this.token = null;
 
 	socket.on('connect', function () {
-		console.log('connected to server');
+		self.emitEvent('rcjs:connected');
 
 		socket.on('rcjs:startCapture', function (data) {
 			self.key = data.key;
 			capture(true, data.events);
+			self.emitEvent('rcjs:startCapture');
 		});
 
 		socket.on('rcjs:receiverDisconnect', function (data) {
-			console.log('receiver disconnected');
+			capture(false);
+			self.emitEvent('rcjs:receiverDisconnect');
 		});
 
 		socket.on('rcjs:supplyToken', function (data) {
 			if (data.error) {
-				console.log(data.error);
-				this.emit('rcjs:error', { msg: data.error } );
+				self.emitEvent('rcjs:error', { msg: data.error } );
 			}
 		});
 
 		socket.on('disconnect', function (data) {
-			//console.log(data.error);
+			self.emitEvent('rcjs:serverDisconnect');
 		});
 	});
 
