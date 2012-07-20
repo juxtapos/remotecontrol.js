45a50b17320cd97c970ce8d2229ba9ed88ab5f9a
juxtapos <claus@formatvorlage.de>
2012-07-20 23:50:02 +0200

    fixed typo

--- web/js/events.js
+++ web/js/events.js
@@ -25,7 +25,7 @@ var EventHandler = {
 		if (this.events[type]) {
 			for (var i = 0, 
 					 et = this.events[type], 
-					 etl = et.length; i < l; i++) {
+					 etl = et.length; i < etl; i++) {
 				if (et[i] === handler) {
 					this.events[type] = et.slice(0, i == 0 ? 0 : i  >= etl ? etl - 1 : i)
 									 	  .concat(et.slice(i + 1, etl));
